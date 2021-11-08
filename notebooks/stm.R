d8944a8b93ca84315ac7cae19854448074677a4c
Bocsárdi Gergő <bocsardi.gergely@gmail.com>
2021-11-08 14:04:22 +0100

    first stm trial

--- notebooks/stm.R
+++ notebooks/stm.R
@@ -5,7 +5,6 @@ library(dplyr)
 
 games_data <- read.csv('./data/clean_games_data.csv')
 
-dummy <- head(games_data, n=5)
 
 # Function to establish own stopwords that appear too many times
 # (based on frequency)
@@ -20,11 +19,21 @@ get_word_freq <- function(dataframe, n = 25) {
   return(c(freq_list[1:n,1]))
 }
 
-summary(dummy)
-
-stm_data <- dummy[c('clean_text', 'location', 'Category', 'launch_date')]
 
 
-processed <- stm::textProcessor(stm_data$clean_text, metadata = stm_data, lowercase = FALSE, removepunctuation = FALSE, stem=FALSE, sparselevel = 0.98, customstopwords = get_word_freq(stm_data))
+stm_data <- games_data[c('clean_text', 'location', 'Category', 'launch_date')]
 
 
+processed <- stm::textProcessor(stm_data$clean_text, metadata = stm_data, lowercase = FALSE, removepunctuation = FALSE, stem=FALSE, sparselevel = 0.997, customstopwords = get_word_freq(stm_data))
+
+stm::plotRemoved(processed$documents, lower.thresh = seq(1, 350, by = 50))
+
+
+
+out <- stm::prepDocuments(processed$documents, processed$vocab, processed$meta, lower.thresh = 100, upper.thresh = 2800)
+
+docs <- out$documents
+vocab <- out$vocab
+meta <- out$meta
+
+games_test_fit <- stm::stm(documents = out$documents, vocab = out$vocab, K = 20, prevalence =~ location + Category + launch_date, max.em.its = 75, data = out$meta, init.type = "Spectral")
