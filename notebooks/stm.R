0639e0e30f6e081097bc1521eaaaac5b55b946f4
Bocsárdi Gergő <bocsardi.gergely@gmail.com>
2021-11-08 13:20:24 +0100

    safety commit

--- notebooks/stm.R
+++ notebooks/stm.R
@@ -1,6 +1,7 @@
 # setting the working directory to my local computer
 setwd('C:/Gergo-mappa/projects/programming/projects/jads/sbm/jads_2021_g9_sbm')
 
+library(dplyr) 
 
 games_data <- read.csv('./data/clean_games_data.csv')
 
@@ -14,14 +15,16 @@ get_word_freq <- function(dataframe, n = 25) {
     tidytext::unnest_tokens(word, clean_text)
   
   freq_list <- word_list %>% 
-    count(word, sort = TRUE)
+    dplyr::count(word, sort = TRUE)
   
   return(c(freq_list[1:n,1]))
 }
 
 summary(dummy)
 
-stm_data <- dummy[c('clean_text', 'location', 'Category', )]
+stm_data <- dummy[c('clean_text', 'location', 'Category', 'launch_date')]
 
+
+processed <- stm::textProcessor(stm_data$clean_text, metadata = stm_data, lowercase = FALSE, removepunctuation = FALSE, stem=FALSE, sparselevel = 0.98, customstopwords = get_word_freq(stm_data))
 
 
