82901e84d5d1511cafb2cb8a1ea92ab5d04936f4
Bocsárdi Gergő <bocsardi.gergely@gmail.com>
2021-11-09 17:03:27 +0100

    stm diagnostics ready, tomorrow is running day

--- notebooks/stm.R
+++ notebooks/stm.R
@@ -1,7 +1,16 @@
 # setting the working directory to my local computer
 setwd('C:/Gergo-mappa/projects/programming/projects/jads/sbm/jads_2021_g9_sbm')
 
-library(dplyr) 
+library(stm)
+library(dplyr)
+library(tidytext)
+library(bigrquery)
+library(purrr)
+library(tidyr)
+library(ggplot2)
+library(furrr)
+library(stminsights)
+library(quanteda)
 
 games_data <- read.csv('./data/clean_games_data.csv')
 
@@ -19,18 +28,15 @@ get_word_freq <- function(dataframe, n = 25) {
   return(c(freq_list[1:n,1]))
 }
 
-
-
 stm_data <- games_data[c('clean_text', 'location', 'Category', 'launch_date')]
 
 
-processed <- stm::textProcessor(stm_data$clean_text, metadata = stm_data, lowercase = FALSE, removepunctuation = FALSE, stem=FALSE, sparselevel = 0.997, customstopwords = get_word_freq(stm_data))
+processed <- textProcessor(stm_data$clean_text, metadata = stm_data, lowercase = FALSE, removepunctuation = FALSE, stem=FALSE, sparselevel = 0.997, customstopwords = get_word_freq(stm_data))
 
-stm::plotRemoved(processed$documents, lower.thresh = seq(1, 350, by = 50))
+plotRemoved(processed$documents, lower.thresh = seq(1, 350, by = 50))
 
 
-
-out <- stm::prepDocuments(processed$documents, processed$vocab, processed$meta, lower.thresh = 100, upper.thresh = 2800)
+out <- prepDocuments(processed$documents, processed$vocab, processed$meta, lower.thresh = 100, upper.thresh = 2800)
 
 docs <- out$documents
 vocab <- out$vocab
@@ -38,17 +44,49 @@ meta <- out$meta
 
 print(paste("First heuristic to determine the number of topics:",round(sqrt(length(games_data$clean_text)/2),0)))
 
-
-timea<-Sys.time()
-many_models <- data_frame(K = c(10, 20, 50, 70, 90, 100, 125, 150,200)) %>%
+many_models <- data_frame(K = c(20, 50, 100, 150)) %>%
   mutate(topic_model = furrr::future_map(K, ~stm::stm(documents=docs, vocab=vocab, K = ., max.em.its = 10,
                                           verbose = FALSE)))
 
-print(Sys.time()-timea)
+heldout <- make.heldout(documents=docs, vocab=vocab)
 
-save(many_models, file = 'num_topics_df.Rda')
+k_result <- many_models %>%
+  mutate(exclusivity = map(topic_model, exclusivity),
+         semantic_coherence = map(topic_model, semanticCoherence, documents=docs),
+         eval_heldout = map(topic_model, eval.heldout, heldout$missing),
+         residual = map(topic_model, checkResiduals, documents=docs),
+         bound =  map_dbl(topic_model, function(x) max(x$convergence$bound)),
+         lfact = map_dbl(topic_model, function(x) lfactorial(x$settings$dim$K)),
+         lbound = bound + lfact,
+         iterations = map_dbl(topic_model, function(x) length(x$convergence$bound)))
+
+k_result %>%
+  transmute(K,
+            `Lower bound` = lbound,
+            Residuals = map_dbl(residual, "dispersion"),
+            `Semantic coherence` = map_dbl(semantic_coherence, mean),
+            `Held-out likelihood` = map_dbl(eval_heldout, "expected.heldout")) %>%
+  gather(Metric, Value, -K) %>%
+  ggplot(aes(K, Value, color = Metric)) +
+  geom_line(size = 1.5, alpha = 0.7, show.legend = FALSE) +
+  facet_wrap(~Metric, scales = "free_y") +
+  labs(x = "K (number of topics)",
+       y = NULL,
+       title = "Model diagnostics by number of topics",
+       subtitle = "These diagnostics indicate that a good number of topics would be a number around 50")
 
 
+k_result %>%
+  select(K, exclusivity, semantic_coherence) %>%
+  filter(K %in% c(20, 50, 100, 150)) %>%
+  unnest() %>%
+  mutate(K = as.factor(K)) %>%
+  ggplot(aes(semantic_coherence, exclusivity, color = K)) +
+  geom_point(size = 2, alpha = 0.7) +
+  labs(x = "Semantic coherence",
+       y = "Exclusivity",
+       title = "Comparing exclusivity and semantic coherence",
+       subtitle = "Models with fewer topics have higher semantic coherence for more topics, but lower exclusivity")
 
 
-games_test_fit <- stm::stm(documents = out$documents, vocab = out$vocab, K = 42, prevalence =~ location + Category + launch_date, max.em.its = 250, reportevery = 1, data = out$meta, init.type = "Spectral")
+games_test_fit <- stm(documents = out$documents, vocab = out$vocab, K = 50, prevalence =~ location + Category + launch_date, max.em.its = 20, reportevery = 5, data = out$meta, init.type = "Spectral")
